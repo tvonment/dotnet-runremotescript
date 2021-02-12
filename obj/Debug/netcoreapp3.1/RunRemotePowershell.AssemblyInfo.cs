4375f2dc89bbc5ca3a0600f98e5c7d1acf33ea8a
Thomas von Mentlen <thomas.vonmentlen@corporatesoftware.ch>
2021-02-12 10:57:42 +0000

    add Docker

--- obj/Debug/netcoreapp3.1/RunRemotePowershell.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/RunRemotePowershell.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("cebb00e2-ad70-4437-8f62-a517d0da3a03")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("RunRemotePowershell")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
